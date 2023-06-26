9450419d9dd40ef1c5af2a083d70b110832fdced
Tyler Vu Van <tv3433@princeton.edu>
2023-06-26 16:23:24 +0700

    registration success

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
     // Connection parameters
     $host = 'localhost';
-    $db = 'users';
+    $db = 'postgres';
     $user = 'postgres';
     $password = '';
-    $port = '5300';
+    $port = '5432';
 ?>
--- register.php
+++ register.php
@@ -1,11 +1,11 @@
 <?php
-require 'database.php';
+$pdo = require 'database.php';
 
 if (isset($_POST['register'])) {
     $username = $_POST['username'];
     $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
     
-    $sql = "INSERT INTO users (username, password) VALUES (?, ?, ?)";
+    $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
     $stmt = $pdo->prepare($sql);
     $stmt->execute([$username, $password]);
     
